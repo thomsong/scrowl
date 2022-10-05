2020da3672b174cc3ce45e72ecab12c2be170221
Greg Thomson <greg@osg.ca>
2022-10-05 10:22:50 -0400

    Reactor initUI

--- packages/editor/src/index.tsx
+++ packages/editor/src/index.tsx
@@ -3,44 +3,13 @@ import { Provider } from "react-redux";
 
 import ClientProxy from "./components/SlideRenderer/SlideHost/ClientProxy";
 import { store } from "./store";
+import { actions as uiActions } from "./store/slices/ui";
 import Editor from "./Editor";
 
-const rootEl = document.getElementById("root");
-const _window = window as any;
-
 (async () => {
-  _window.nativeTheme = await _window.ScrowlApp.getNativeTheme();
-  _window.prefersReducedMotion = _window.ScrowlApp.prefersReducedMotion();
+  await uiActions.initUI();
 
-  // This is an ugly hack, but works for now until it's moved into the UI store
-  (async () => {
-    const htmlRoot = document.getElementsByTagName("html")[0];
-
-    // Prevent infinite loop
-    let loopCount = 0;
-    while (loopCount++ < 100) {
-      if (_window.nativeTheme.shouldUseHighContrastColors) {
-        htmlRoot.classList.add("high-contrast");
-      } else {
-        htmlRoot.classList.remove("high-contrast");
-      }
-
-      if (false && _window.nativeTheme.shouldUseDarkColors) {
-        // This will be used for future dark-theme
-        htmlRoot.classList.add("dark");
-        htmlRoot.classList.remove("light");
-      } else {
-        htmlRoot.classList.add("light");
-        htmlRoot.classList.remove("dark");
-      }
-
-      // This will wait here until there is a change... maybe forever
-      _window.nativeTheme = await _window.ScrowlApp.listenForThemeUpdate();
-
-      _window.prefersReducedMotion = await _window.ScrowlApp.prefersReducedMotion();
-    }
-  })();
-
+  const rootEl = document.getElementById("root");
   if (rootEl) {
     ClientProxy.init();
     ReactDOM.createRoot(rootEl).render(<Provider store={store}>{<Editor />}</Provider>);
--- packages/editor/src/store/slices/ui/index.ts
+++ packages/editor/src/store/slices/ui/index.ts
@@ -87,6 +87,46 @@ const initialState: state = {
   ],
 };
 
+const initUI = createAsyncThunk(
+  //action type string
+  "ui/initUI",
+  async () => {
+    const _window = window as any;
+    _window.nativeTheme = await _window.ScrowlApp.getNativeTheme();
+    _window.prefersReducedMotion = _window.ScrowlApp.prefersReducedMotion();
+
+    (async () => {
+      const htmlRoot = document.getElementsByTagName("html")[0];
+
+      // Prevent infinite loop
+      let loopCount = 0;
+      while (loopCount++ < 100) {
+        if (_window.nativeTheme.shouldUseHighContrastColors) {
+          htmlRoot.classList.add("high-contrast");
+        } else {
+          htmlRoot.classList.remove("high-contrast");
+        }
+
+        if (false && _window.nativeTheme.shouldUseDarkColors) {
+          // This will be used for future dark-theme
+          htmlRoot.classList.add("dark");
+          htmlRoot.classList.remove("light");
+        } else {
+          htmlRoot.classList.add("light");
+          htmlRoot.classList.remove("dark");
+        }
+
+        // This will wait here until there is a change... maybe forever
+        _window.nativeTheme = await _window.ScrowlApp.listenForThemeUpdate();
+
+        _window.prefersReducedMotion = await _window.ScrowlApp.prefersReducedMotion();
+      }
+    })();
+
+    return true;
+  }
+);
+
 const launchPreview = createAsyncThunk(
   //action type string
   "ui/launchPreview",
@@ -419,5 +459,6 @@ export const actions = {
   handleGlobalAction,
   showOverlay,
   launchPreview,
+  initUI,
 };
 export default slice.reducer;
